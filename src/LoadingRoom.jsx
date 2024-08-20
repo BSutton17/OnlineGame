c0dc88646b87a3ebd49d989a0e0e9935765fc5ad
Bryson Sutton <139702261+BSutton17@users.noreply.github.com>
2024-08-19 19:46:26 -0500

    Update LoadingRoom.jsx

--- src/LoadingRoom.jsx
+++ src/LoadingRoom.jsx
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import App from "./App";
 import './App.css'
 
-const socket = io.connect("http://localhost:3001")
+const socket = io.connect("https://online-game-server.vercel.app/")
 
 const LoadingRoom = () =>{
     const [userName, setUsername] = useState("");
